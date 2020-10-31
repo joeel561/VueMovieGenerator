76fe3508f854882287040083a0b353aaabb62d2f
leNicDev <germanelectronix@gmail.com>
2020-10-31 22:30:26 +0100

    Works for me lel

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -14,7 +14,8 @@
                 params: {
                     api_key: 'f4c26714ba3f4672f618f331893d064b',
                     with_genres: 27,
-                    vote_average.gte
+                    'vote_average.gte': 10,
+                    'primary_release_date.gte': '2020-10-10'
                 },
                 URL: 'https://api.themoviedb.org/3/discover/movie',
                 result: {}
